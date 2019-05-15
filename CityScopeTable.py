f85df0701337db14f7882ae12f23a35129f1ba80
andredaa <post.for.andre@gmail.com>
2019-05-15 17:17:54 +0200

    Flip table origin from SouthEast to NorthWest, for flipped tables

--- CityScopeTable.py
+++ CityScopeTable.py
@@ -6,6 +6,7 @@ from shapely.geometry import Point
 import json
 import urllib
 import pyproj
+import math
 
 
 class CityScopeTable:
@@ -16,18 +17,11 @@ class CityScopeTable:
         self.table_flipped = table_flipped
 
         self.result = json.load(urllib.urlopen(self.address))
-        self.start_cell_origin = (Point(self.result['header']['spatial']['latitude'], self.result['header']['spatial']['longitude']))
+        self.start_cell_origin = (Point(self.result['header']['spatial']['longitude'], self.result['header']['spatial']['latitude']))
         self.table_rotation = self.result['header']['spatial']['rotation']  # TODO can the table rotation be different form the cell rotation??
         self.table_cell_size = self.result['header']['spatial']['cellSize']
         self.table_row_count = self.result['header']['spatial']['nrows']
         self.table_column_count = self.result['header']['spatial']['ncols']
-
-    def create_origin(self, table_flipped):
-        if not table_flipped:
-            self.start_cell_origin = (Point(self.result['header']['spatial']['latitude'], self.result['header']['spatial']['longitude']))
-            return self.start_cell_origin
-
-
 
     def get_result(self):
         return self.result
@@ -36,20 +30,12 @@ class CityScopeTable:
         origin = self.get_reprojected_origin()
         if not self.table_flipped:
             return origin
-        # else: translate the table origin from the Northwest to the south east corner
-        # TODO rotation berücksichtigen!"!
-        flipped_x = origin.x - (self.get_table_column_count() * self.table_cell_size)
-        flipped_y = origin.y - (self.get_table_row_count() * self.table_cell_size)
 
-        print(flipped_x, flipped_y)
-
-        return Point(flipped_x, flipped_y)
+        # else: shift the table origin from the SouthEast corner to the NorthWest corner
+        return self.get_flipped_origin(origin)
 
     def get_table_rotation(self):
-        if self.table_flipped:
-            return 360 - self.table_rotation
-
-        return 360- self.table_rotation
+        return 360 - self.table_rotation
 
     def get_table_cell_size(self):
         return self.table_cell_size
@@ -66,3 +52,17 @@ class CityScopeTable:
         origin_x, origin_y = pyproj.transform(wgs84, espg, self.start_cell_origin.x, self.start_cell_origin.y)
 
         return Point(origin_x, origin_y)
+
+    # returns the opposite corner of the grid (SE->NW)
+    def get_flipped_origin(self, origin):
+        table_width = self.get_table_column_count() * self.table_cell_size
+        table_height = self.get_table_row_count() * self.table_cell_size
+        diagonal = math.sqrt(pow(table_width, 2) + pow(table_height, 2))
+        diagonal_angle = math.degrees(math.atan(table_width/table_height))
+        angle_diagonal_to_x_axis = diagonal_angle - self.get_table_rotation()
+        delta_x = math.sin(math.radians(angle_diagonal_to_x_axis)) * diagonal
+        delta_y = math.cos(math.radians(angle_diagonal_to_x_axis)) * diagonal
+        flipped_x = origin.x - delta_x
+        flipped_y = origin.y + delta_y
+
+        return Point(flipped_x, flipped_y)
