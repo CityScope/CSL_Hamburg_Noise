0bee5d6311bfed70ca8e56f590e223b91f96b6bb
andredaa <post.for.andre@gmail.com>
2019-12-17 14:55:38 +0100

    do not force logs when multiple instances start

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,7 +4,7 @@ if [ "$#" -gt 0 ]; then # if command line arguments were given
     docker stop noise_module_instance_$1
     docker rm noise_module_instance_$1
     docker run --name noise_module_instance_$1 -d noise_module $1
-    docker logs -f noise_module_instance_$1
+    # docker logs -f noise_module_instance_$1  ## do not force logs when multiple instances start
 
 else # no command line args -> don't choose endpoint
     docker stop noise_module_instance
